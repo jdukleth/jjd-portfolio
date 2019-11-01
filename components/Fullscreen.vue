acb317742ccefe031dc65d49ab87f43eee514968
Jason Dukleth <jason@dukleth.com>
2019-10-31 23:19:24 -0500

    adds Fullscreen button & additional app responsiveness

---
+++ components/Fullscreen.vue
@@ -0,0 +1,67 @@
+<template>
+  <div v-if="$vuetify.breakpoint.mdAndUp">
+    <v-btn icon class="ma-2 pa-7" @click="toggleFullscreen">
+      <v-icon size="36">
+        {{ isFullscreen ? 'mdi-fullscreen-exit' : 'mdi-fullscreen' }}
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isFullscreen: false
+    }
+  },
+
+  methods: {
+    toggleFullscreen(event) {
+      const elem = document.documentElement
+
+      if (
+        document.fullscreenEnabled ||
+        document.webkitFullscreenEnabled ||
+        document.mozFullScreenEnabled ||
+        document.msFullscreenEnabled
+      ) {
+        if (!this.isFullscreen) {
+          if (elem.requestFullscreen) {
+            elem.requestFullscreen()
+            this.isFullscreen = true
+          } else if (elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen()
+            this.isFullscreen = true
+          } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen()
+            this.isFullscreen = true
+          } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen()
+            this.isFullscreen = true
+          }
+        } else if (document.exitFullscreen) {
+          document.exitFullscreen()
+          this.isFullscreen = false
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen()
+          this.isFullscreen = false
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen()
+          this.isFullscreen = false
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen()
+          this.isFullscreen = false
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-btn {
+  height: auto; /* necessary for Chrome; */
+  width: auto; /* fixed dimensions mess up alignment */
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,30 @@
 <template>
-  <v-app dark>
+  <v-app
+    dark
+    :class="{
+      xs: $vuetify.breakpoint.xs,
+      sm: $vuetify.breakpoint.sm,
+      md: $vuetify.breakpoint.md,
+      lg: $vuetify.breakpoint.lg,
+      xl: $vuetify.breakpoint.xl
+    }"
+  >
     <Stars v-cloak id="Stars" />
-    <nuxt v-cloak id="Page" class="mb-12" />
+    <Fullscreen id="Fullscreen" />
+    <nuxt v-cloak id="Page" />
     <Navbar v-cloak />
   </v-app>
 </template>
 
 <script>
 import Stars from '../components/Stars'
+import Fullscreen from '../components/Fullscreen'
 import Navbar from '../components/Navbar'
 
 export default {
   components: {
     Stars,
+    Fullscreen,
     Navbar
   },
 
@@ -27,11 +39,38 @@ export default {
 </script>
 
 <style lang="scss">
-html,
-body {
+/* *************************************** */
+/*  Vuetify Breakpoint App-Level Styles    */
+/*           *  -16px on Desktop           */
+/* *************************************** */
+// < 600px
+.xs {
+  font-size: 12px;
+}
+
+// 600px > < 960px
+.sm {
   font-size: 16px;
 }
 
+// 960px > < 1264px*
+.md {
+  font-size: 20px;
+}
+
+// 1264px* > < 1904px*
+.lg {
+  font-size: 24px;
+}
+
+// > 1904px*
+.xl {
+  font-size: 28px;
+}
+
+/* *************************************** */
+/*  Section Styles                         */
+/* *************************************** */
 #Stars {
   z-index: 1;
 }
@@ -43,6 +82,16 @@ body {
   height: calc(100vh - 65px);
 }
 
+#Fullscreen {
+  z-index: 3;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
+/* *************************************** */
+/*  Vuetify App-Level Style Fixes          */
+/* *************************************** */
 .theme--dark.v-application {
   background: #000000;
 }
