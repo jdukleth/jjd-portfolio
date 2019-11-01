6ffeb056062ffee558c89a0edb52eb79c05026d2
Jason Dukleth <jason@dukleth.com>
2019-11-01 04:25:43 -0500

    adds skeleton pages for each section

---
+++ pages/work.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fluid class="ma-0 pa-0">
+    <v-row class="full-height" align="center" justify="center">
+      <v-col md="auto">
+        <h1>Work</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// import Logo from '../components/Logo'
+// import Nameplate from '../components/Nameplate'
+
+export default {
+  components: {
+    // Logo,
+    // Nameplate
+  }
+}
+</script>
+
+<style scoped></style>
