e254087ed5031a8024e68dacf8ac75d96e6ab3b9
Jason Dukleth <jason@dukleth.com>
2019-11-02 16:51:24 -0500

    creates quote component

---
+++ components/Quote.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-alert
+    color="transparent"
+    dark
+    icon="mdi-format-quote-open"
+    border="left"
+    prominent
+  >
+    <slot></slot>
+  </v-alert>
+</template>
+
+<style lang="scss" scoped>
+.v-alert {
+  background-color: rgba(122, 122, 122, 0.05) !important;
+  color: slategray;
+  font-size: 1em;
+  letter-spacing: 0.05em;
+}
+</style>
