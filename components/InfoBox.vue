e99fd6a607a8cda56a7bbfc615e2921ed74dd95b
Jason Dukleth <jason@dukleth.com>
2019-11-05 19:32:51 -0600

    creates InfoBox component

---
+++ components/InfoBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-alert color="transparent" dark prominent class="pa-6">
+    <slot></slot>
+  </v-alert>
+</template>
+
+<style lang="scss" scoped>
+.v-alert {
+  background-color: rgba(122, 122, 122, 0.05) !important;
+  color: slategray;
+  font-size: 1em;
+  letter-spacing: 0.05em;
+}
+</style>
